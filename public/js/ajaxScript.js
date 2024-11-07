969ec510f1008307dce85acb06fb80ab12728d78
I'm Just Ken <126956034+chimkem@users.noreply.github.com>
2024-11-07 03:44:07 +0200

    public
    I had to download these separately since github didn't want to work on my computer.

---
+++ public/js/ajaxScript.js
@@ -0,0 +1,42 @@
+function getAjaxMessage(event) {
+    event.preventDefault(); // Disable the default form behaviour
+    var date = new Date().toLocaleString()
+    // Get information from the form
+    const ajaxMessage = {
+        username: $('#username').val(),
+        country: $('#country').val(),
+        message: $('#message').val(),
+    };
+
+    // Don't send the form if it doesn't have all the information
+    if (!ajaxMessage.username || 
+        !ajaxMessage.country || 
+        !ajaxMessage.message) {
+        return;
+    };
+
+    $.ajax({
+        url: '/ajaxmessage', 
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(ajaxMessage),
+        // Create the div with the given information
+        success: function(data) {
+            const newMessage = `
+              <div>
+                <p><strong>Username:</strong> ${data[0].username} // 
+                <strong>Country:</strong> ${data[0].country} // 
+                <strong>Message:</strong> ${data[0].message} // 
+                <strong>Time:</strong> ${date}</p>
+              <div>
+            `;
+            // Add the message to the page and reset the form
+            $('#ajaxList').append(newMessage); 
+            $('#ajaxForm')[0].reset();
+        },
+        // In case of error
+        error: function(status, error) {
+            console.error('Error:', status, error);
+        }
+    });
+}
